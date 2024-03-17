0004b32dc68d73da56618c33dac66953b9617945
Dudu <aeiou7352@gmail.com>
2024-03-17 22:34:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,109 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3001;
+
+const perks = [
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    level: "intermediate",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
+    level: "intermediate",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg",
+    level: "intermediate",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    level: "intermediate",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg",
+    level: "beginner",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rust/rust-original.svg",
+    level: "beginner",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg",
+    level: "beginner",
+  },
+  {
+    imageUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg",
+    level: "beginner"
+  }
+];
+
+const userInfo = {
+  name: 'Jakub Duda',
+  paragraph: 'I am a passionate developer fluent in spring, react, motivated enough to learn new technologies. Student of Piotr Drzewiecki technical school in Poland with a deep love for a software development',
+  profilePicture: 'https://source.unsplash.com/random/800x800?query=portrait',
+};
+
+const articles = [
+  {
+    head: 'About me',
+    content: 'As a 17 years old software developer I have been interested in coding since my youngest age. This deep affection to field has led me to choose Piotr Drzewiecki technical school, which is one of the best technical schools in the Poland. I love to code both professionally and as a hobby. This interest has also led me to creating my own Github account, on which I frequently contribute code to many different projects.',
+    imageUrl: 'https://source.unsplash.com/random/900x600',
+    link: {
+      url: 'https://www.google.com',
+      text: 'Check out my github profile'
+    }
+  },
+  {
+    head: 'Education',
+    content: 'I am currently attending the Piotr Drzewiecki technical school in Poland.',
+    imageUrl: 'https://source.unsplash.com/random/900x600?query=education',
+    link: {
+      url: 'https://www.tm1.edu.pl',
+      text: 'Read more'
+    }
+  },
+  {
+    head: 'My knowledge',
+    content: 'Thanks to embracing the software develompent as my passion, I have learnt multiple languages such as Kotlin, Java, Javascript, Typescript, Python, Rust and several libraries/frameworks, including Spring Boot, React, Actix Web and Jetpack Compose. From hackathons to internship, I always chose to face the challenges, grow and learn. As a result, my skill and deep understanding of them is much better and clearer.',
+    imageUrl: 'https://source.unsplash.com/random/900x600?query=knowledge'
+  },
+  {
+    head: 'Internship in SAS media',
+    content: 'During the internship in SAS media, I have been learnt how to use Jetpack Compose and have been introduced to material 3 design language and it\'s guidelines. Thorought my work I have learnt how to work efficiently in a team. Overall, thanks to this internship, I have been able to get a grasp of software engineer job, it\'s requirements, and mechanics.',
+    imageUrl: 'https://source.unsplash.com/random/900x600?query=internship',
+    link: {
+      url: 'https://sas-media.pl/',
+      text: 'More about SAS media'
+    }
+  },
+  {
+    head: 'AIChan',
+    content: 'As a one of group projects in the technical school, my team had chosen to create a social media site fully populated by bots, using the OpenAI API, Spring Boot and Angular. This tech stack has allowed us to understand how the OpenAI API works and how one should assign work inside the team. Since then, we have made incremental improvements to the project, with the plans to monetize the site in the near future.',
+    imageUrl: 'https://source.unsplash.com/random/900x600?query=AI',
+    link: {
+      url: 'https://github.com/AIChan-Team',
+      text: 'Read more about AIChan'
+    }
+  }
+];
+
+app.use(cors());
+
+app.get('/', (_, res) => {
+  res.json(articles);
+});
+
+app.get('/user', (_, res) => {
+  res.json(userInfo);
+});
+
+app.get('/perks', (_, res) => {
+  res.json(perks.sort((a, b) => a.level > b.level));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
